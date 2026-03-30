e4ec68e59d33947746f41e65d12b53b27924e111
AnikethMungara <aniketh.official@gmail.com>
2026-03-29 23:20:33 -0700

    submission.cs

--- Submission.cs
+++ Submission.cs
@@ -4,12 +4,8 @@ using System.Xml;
 using Newtonsoft.Json;
 using System.IO;
 using System.Collections.Generic;
+using System.Net;
 
-/**
- * This template file is created for ASU CSE445 Distributed SW Dev Assignment 4.
- * Please do not modify or delete any existing class/variable/method names. However, you can add more variables and functions.
- * Uploading this file directly will not pass the autograder's compilation check, resulting in a grade of 0.
- * **/
 namespace ConsoleApp1
 {
     public class Submission
@@ -33,72 +29,63 @@ namespace ConsoleApp1
         // Q2.1
         public static string Verification(string xmlUrl, string xsdUrl)
         {
-            // return "No errors are found" if XML is valid. Otherwise, return the desired exception message.
+            List<string> errors = new List<string>();
+
             try
             {
-                string xmlContent = DownloadContent(xmlUrl);
-                string xsdContent = DownloadContent(xsdUrl);
-
                 XmlSchemaSet schemaSet = new XmlSchemaSet();
-                using (StringReader xsdReader = new StringReader(xsdContent))
+
+                XmlReaderSettings xsdReaderSettings = new XmlReaderSettings();
+                xsdReaderSettings.DtdProcessing = DtdProcessing.Ignore;
+                using (XmlReader xsdReader = XmlReader.Create(xsdUrl, xsdReaderSettings))
                 {
-                    schemaSet.Add(null, XmlReader.Create(xsdReader));
+                    XmlSchema schema = XmlSchema.Read(xsdReader, null);
+                    schemaSet.Add(schema);
                 }
 
-                List<string> errors = new List<string>();
-
                 XmlReaderSettings settings = new XmlReaderSettings();
-                settings.Schemas = schemaSet;
                 settings.ValidationType = ValidationType.Schema;
+                settings.Schemas = schemaSet;
+                settings.DtdProcessing = DtdProcessing.Ignore;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+
                 settings.ValidationEventHandler += (sender, e) =>
                 {
                     errors.Add(e.Message);
                 };
 
-                using (StringReader xmlStringReader = new StringReader(xmlContent))
-                using (XmlReader reader = XmlReader.Create(xmlStringReader, settings))
+                using (XmlReader reader = XmlReader.Create(xmlUrl, settings))
                 {
                     while (reader.Read()) { }
-                }
-
-                if (errors.Count == 0)
-                {
-                    return "No errors are found";
-                }
-                else
-                {
-                    return string.Join("\n", errors);
                 }
             }
             catch (Exception ex)
             {
-                return ex.Message;
+                errors.Add(ex.Message);
             }
+
+            if (errors.Count == 0)
+            {
+                return "No errors are found";
+            }
+
+            return string.Join("\n", errors);
         }
 
         // Q2.2
         public static string Xml2Json(string xmlUrl)
         {
-            // The returned jsonText needs to be de-serializable by Newtonsoft.Json package.
-            // (JsonConvert.DeserializeXmlNode(jsonText))
             try
             {
                 XmlDocument doc = new XmlDocument();
                 doc.Load(xmlUrl);
-                return JsonConvert.SerializeXmlNode(doc.DocumentElement, Formatting.Indented);
+                string jsonText = JsonConvert.SerializeXmlNode(doc.DocumentElement, Newtonsoft.Json.Formatting.Indented);
+
+                return jsonText;
             }
             catch (Exception ex)
             {
                 return ex.Message;
-            }
-        }
-
-        // Helper method to download content from URL
-        private static string DownloadContent(string url)
-        {
-            using (System.Net.WebClient client = new System.Net.WebClient())
-            {
-                return client.DownloadString(url);
             }
         }
     }
