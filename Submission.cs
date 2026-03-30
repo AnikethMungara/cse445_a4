ceac2d7a346dbdd4c0a972c3c89d929ab28aa48d
AnikethMungara <aniketh.official@gmail.com>
2026-03-29 19:24:26 -0700

    attempt

--- Submission.cs
+++ Submission.cs
@@ -4,96 +4,78 @@ using System.Xml;
 using Newtonsoft.Json;
 using System.IO;
 
-
-
-/**
- * This template file is created for ASU CSE445 Distributed SW Dev Assignment 4.
- * Please do not modify or delete any existing class/variable/method names. However, you can add more variables and functions.
- * Uploading this file directly will not pass the autograder's compilation check, resulting in a grade of 0.
- * **/
-
-
 namespace ConsoleApp1
 {
-
-
     public class Submission
     {
-        public static string xmlURL = "https://raw.githubusercontent.com/AnikethMungara/cse445_a4/main/NationalParks.xml";
-        public static string xmlErrorURL = "https://raw.githubusercontent.com/AnikethMungara/cse445_a4/main/NationalParksErrors.xml";
-        public static string xsdURL = "https://raw.githubusercontent.com/AnikethMungara/cse445_a4/main/NationalParks.xsd";
+        public static string xmlURL = "https://anikethmungara.github.io/cse445_a4/NationalParks.xml";
+        public static string xmlErrorURL = "https://anikethmungara.github.io/cse445_a4/NationalParksErrors.xml";
+        public static string xsdURL = "https://anikethmungara.github.io/cse445_a4/NationalParks.xsd";
 
         public static void Main(string[] args)
         {
             string result = Verification(xmlURL, xsdURL);
             Console.WriteLine(result);
 
-
             result = Verification(xmlErrorURL, xsdURL);
             Console.WriteLine(result);
-
 
             result = Xml2Json(xmlURL);
             Console.WriteLine(result);
         }
 
-        // Q2.1
         public static string Verification(string xmlUrl, string xsdUrl)
         {
+            System.Text.StringBuilder errors = new System.Text.StringBuilder();
+
             try
             {
-                string xsdContent = DownloadContent(xsdUrl);
-                XmlSchemaSet schemas = new XmlSchemaSet();
-                schemas.Add(null, XmlReader.Create(new StringReader(xsdContent)));
-
-                string xmlContent = DownloadContent(xmlUrl);
+                XmlSchemaSet schemaSet = new XmlSchemaSet();
+                schemaSet.Add(null, xsdUrl);
 
                 XmlReaderSettings settings = new XmlReaderSettings();
                 settings.ValidationType = ValidationType.Schema;
-                settings.Schemas = schemas;
-
-                string errors = "";
+                settings.Schemas = schemaSet;
 
                 settings.ValidationEventHandler += (sender, e) =>
                 {
-                    if (errors != "")
-                        errors += "\n";
-                    errors += e.Message;
+                    errors.AppendLine(e.Message);
                 };
 
-                using (XmlReader reader = XmlReader.Create(new StringReader(xmlContent), settings))
+                using (XmlReader reader = XmlReader.Create(xmlUrl, settings))
                 {
-                    while (reader.Read()) { }
+                    try
+                    {
+                        while (reader.Read()) { }
+                    }
+                    catch (XmlException ex)
+                    {
+                        errors.AppendLine(ex.Message);
+                    }
                 }
-
-                if (errors == "")
-                    return "No errors are found";
-                else
-                    return errors;
             }
             catch (Exception ex)
             {
-                return ex.Message;
+                errors.AppendLine(ex.Message);
+            }
+
+            if (errors.Length == 0)
+            {
+                return "No errors are found";
+            }
+            else
+            {
+                return errors.ToString().Trim();
             }
         }
 
         public static string Xml2Json(string xmlUrl)
         {
-            string xmlContent = DownloadContent(xmlUrl);
             XmlDocument doc = new XmlDocument();
-            doc.LoadXml(xmlContent);
-            string jsonText = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented, true);
+            doc.Load(xmlUrl);
+
+            string jsonText = Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc.DocumentElement, Newtonsoft.Json.Formatting.Indented);
             return jsonText;
         }
-
-        // Helper method to download content from URL
-        private static string DownloadContent(string url)
-        {
-            using (System.Net.WebClient client = new System.Net.WebClient())
-            {
-                return client.DownloadString(url);
-            }
-        }
     }
-
 }
